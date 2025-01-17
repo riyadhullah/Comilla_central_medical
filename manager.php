ac47735d93488a091f8cc89590129a9377a61faf
MinhajunNabi <153017394+MinhajunNabi@users.noreply.github.com>
2025-01-17 19:43:35 +0600

    rough

--- manager.php
+++ manager.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Make Appointment</title>
-    <link rel="stylesheet" href="css\\appointment_style.css">
+    <title>Manager Page</title>
+    <link rel="stylesheet" href="css\\appointment_style.css"> <!-- -----------------change href ---------------- -->
+    
 </head>
 <body>
     <form class="appointment-form" action="submit.php" method="post">
-        <h1>Make Appointment</h1>
+        <h1>Details</h1>
         
         <div class="form-group">
             <label for="fullname">Patient's Full Name : </label>
